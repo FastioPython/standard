c1ba8fa36eedbf0ccde7bc6d7e079132f3f19b32
Quan Vu <huyquan.vu.1@gmail.com>
2021-09-13 22:48:51 +0700

    Add missing code

---
+++ app/Api/V1/endpoints/item.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+router = APIRouter()
+
+fake_items = [
+    {
+        "id": 1,
+        "title": "Foo"
+    },
+    {
+        "id": 2,
+        "title": "Bar"
+    },
+]
+
+@router.get("/")
+async def read_items():
+    return fake_items
+
+
+@router.get("/{id}")
+async def read_item(id: str):
+    for item in fake_items:
+        if item["id"] == id:
+            return item
+    return None
+
+
+@router.put("/{item_id}", 
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def update_item(item_id: str):
+    if item_id != "1":
+        raise HTTPException(
+            status_code=403, 
+            detail="You can only update the item: foo"
+        )
+    return {"item_id": item_id, "name": "The Fighters"}
